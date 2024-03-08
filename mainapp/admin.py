f435c68d6d5af1f9633b04dc5f9bfb26af582802
Nikunj <pansuri9@uwindsor.ca>
2024-03-07 21:06:02 -0500

    merger master changes

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,7 +2,5 @@ from django.contrib import admin
 from .models import UserProfile, Place
 
 # Register your models here.
-from .models import UserProfile
-admin.site.register(UserProfile)
 admin.site.register(UserProfile)
 admin.site.register(Place)
