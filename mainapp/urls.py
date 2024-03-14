b44630aa7e267346866234a39dc0141c8d336cc2
metaliyachirag <metaliyc@uwindsor.ca>
2024-03-14 13:30:04 -0400

    Removed trip list form

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -11,6 +11,5 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('signup/', views.user_signup, name='signup'),
     path('logout/', views.user_logout, name='logout'),
-    path('add_trip/', views.add_trip, name='add_trip'),
-    path('trip_list/', views.trip_list, name='trip_list')
+    path('add_trip/', views.add_trip, name='add_trip')
 ]
