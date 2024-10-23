d3c822d457fec4e13ea483e81fd1b09546674a39
Dixit Nandaniya <dixitnandaniya2001@gmail.com>
2024-10-23 19:56:50 -0400

    solved vercel configuration

---
+++ public/static/mainapp/messages.js
@@ -0,0 +1,214 @@
+let input_message = $('#input-message');
+let message_body = $('.msg_card_body');
+let send_message_form = $('#send-message-form');
+const USER_ID = $('#logged-in-user').val();
+
+let loc = window.location;
+let wsStart = 'ws://';
+
+if(loc.protocol === 'https') {
+    wsStart = 'wss://';
+}
+let endpoint = wsStart + loc.host + loc.pathname;
+
+var socket = new WebSocket(endpoint);
+
+socket.onopen = async function(e) {
+    console.log('WebSocket connection opened', e);
+    send_message_form.on('submit', function (e) {
+        e.preventDefault();
+        let message = input_message.val();
+        let receiver_id = get_active_other_user_id();
+        let userchat_id = get_active_userchat_id();
+
+        let data = {
+            'message': message,
+            'sender_id': USER_ID,
+            'receiver_id': receiver_id,
+            'userchat_id': userchat_id
+        };
+        data = JSON.stringify(data);
+        socket.send(data);
+        $(this)[0].reset();
+    });
+};
+
+socket.onmessage = async function(e) {
+    let data;
+    try {
+        data = JSON.parse(e.data);
+    } catch (error) {
+        console.error('Error parsing message:', error);
+        return;
+    }
+    let message = data['message'];
+    let sent_by_id = data['sent_by'];
+    let userchat_id = data['userchat_id'];
+    let send_time = data['send_time'];
+    let sender_username = data['username'];
+    let user_photo = data['user_photo'];
+
+    if (!message || !sent_by_id || !userchat_id) {
+        console.error('Error: Incomplete message data');
+        return;
+    }
+    newMessage(message, sent_by_id, userchat_id, send_time, sender_username, user_photo);
+};
+
+
+socket.onerror = async function(e) {
+    console.error('Websocket error: ', e);
+};
+
+socket.onclose = async function(e) {
+    console.log('close', e);
+};
+
+function newMessage(message, sent_by_id, userchat_id, send_time, sender_username, user_photo) {
+    if ($.trim(message) === '') {
+        return false;
+    }
+    let message_element;
+    let chat_id = 'chat_' + userchat_id;
+
+    console.log(user_photo);
+
+    if(sent_by_id === USER_ID && user_photo != null){
+        message_element = `
+            <div class="d-flex mb-4 replied">
+                <div class="msg_cotainer_send">
+                    ${message}
+                    <span class="msg_time_send">${send_time}</span>
+                </div>
+                <div class="img_cont_msg">
+                    <img src="${user_photo}" class="rounded-circle user_img_msg">
+                </div>
+            </div>
+        `
+    } else if(sent_by_id === USER_ID && user_photo == null){
+        message_element = `
+            <div class="d-flex mb-4 replied">
+                <div class="msg_cotainer_send">
+                    ${message}
+                    <span class="msg_time_send">${send_time}</span>
+                </div>
+                <div class="img_cont_msg">
+                    <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg">
+                </div>
+            </div>
+        `
+    } else if(sent_by_id !== USER_ID && user_photo != null){
+        message_element = `
+            <div class="d-flex mb-4 received">
+                <div class="img_cont_msg">
+                    <img src="${user_photo}" class="rounded-circle user_img_msg">
+                </div>
+                <div class="msg_cotainer">
+                    ${message}
+                    <span class="msg_time">${sender_username}, ${send_time}</span>
+                </div>
+            </div>
+        `;
+    }
+    else {
+        message_element = `
+            <div class="d-flex mb-4 received">
+                <div class="img_cont_msg">
+                    <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg">
+                </div>
+                <div class="msg_cotainer">
+                    ${message}
+                    <span class="msg_time">${sender_username}, ${send_time}</span>
+                </div>
+            </div>
+        `;
+    }
+
+    let message_body = $('.messages-wrapper[chat-id="' + chat_id + '"] .msg_card_body');
+    message_body.append($(message_element));
+    setTimeout(function() {
+        message_body.scrollTop(message_body[0].scrollHeight);
+    }); // Adjust the delay as needed
+
+    input_message.val(null);
+}
+
+
+$('.contact-li').on('click', function (){
+    $('.contacts .active').removeClass('active');
+    $(this).addClass('active');
+
+    let chat_id = $(this).attr('chat-id');
+    $('.messages-wrapper.is_active').removeClass('is_active');
+    let messageWrapper = $('.messages-wrapper[chat-id="' + chat_id +'"]');
+    messageWrapper.addClass('is_active');
+
+    // Scroll to the top of the message body
+    let messageBody = messageWrapper.find('.msg_card_body');
+    messageBody.scrollTop(messageBody[0].scrollHeight);
+
+    let userchat_id = chat_id.replace('chat_', '');
+
+    // Make an AJAX call to mark messages as read
+    $.ajax({
+        url: '/mark_messages_as_read/', // Update this URL to match your URL configuration
+        type: 'POST',
+        data: {
+            'userchat_id': userchat_id,
+            'user_id': USER_ID,
+            'csrfmiddlewaretoken': '{{ csrf_token }}' // Include CSRF token for POST requests
+        },
+        success: function(response) {
+            if (response.success) {
+                console.log('Messages marked as read');
+            } else {
+                console.error('Error marking messages as read:', response.error);
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error('AJAX error:', error);
+        }
+    });
+
+});
+
+function get_active_other_user_id(){
+    let other_user_id = $('.messages-wrapper.is_active').attr('other-user-id');
+    other_user_id = $.trim(other_user_id);
+    return other_user_id;
+}
+
+function get_active_userchat_id(){
+    let chat_id = $('.messages-wrapper.is_active').attr('chat-id');
+    let userchat_id = chat_id.replace('chat_', '');
+    return userchat_id;
+}
+
+$(document).ready(function() {
+    $("#searchbar").on("keyup", function() {
+        var searchText = $(this).val().toLowerCase();
+        $("#contacts .contact-li").each(function() {
+            var username = $(this).find(".user_info span").text().toLowerCase();
+            if (username.indexOf(searchText) === -1) {
+                $(this).hide();
+            } else {
+                $(this).show();
+            }
+        });
+    });
+
+    let activeChat = $('.messages-wrapper.is_active');
+    if (activeChat.length > 0) {
+        let messageBody = activeChat.find('.msg_card_body');
+        messageBody.scrollTop(messageBody[0].scrollHeight);
+    }
+
+    $(".contact-li").click(function() {
+        $(".contact-li").removeClass("active");
+        $(this).addClass("active");
+
+        var chatId = $(this).attr("chat-id");
+        $(".messages-wrapper").removeClass("is_active").addClass("hide");
+        $(".messages-wrapper[chat-id='" + chatId + "']").removeClass("hide").addClass("is_active");
+    });
+});
