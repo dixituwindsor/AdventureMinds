6d151f12bb47175cf2150d94d21d80f52db090e3
Dixit Nandaniya <dixitnandaniya2001@gmail.com>
2024-10-23 20:15:29 -0400

    added configuration for sqlite file

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,8 @@
 echo "BUILD START"
 python3 -m pip install -r requirements.txt
+echo "Fetching db.sqlite3 from GitHub"
+curl -o db.sqlite3 https://raw.githubusercontent.com/dixituwindsor/AdventureMinds/dixit/db.sqlite3
 python3 manage.py collectstatic --noinput --clear
 mv staticfiles_build/static public/
 echo "BUILD END"
+chmod 777 db.sqlite3
