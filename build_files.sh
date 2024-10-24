4629cfff0729b26141198d31b530a4cf85b38fea
Dixit Nandaniya <dixitnandaniya2001@gmail.com>
2024-10-23 20:25:31 -0400

    added configuration for sqlite file

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,11 @@ echo "BUILD START"
 python3 -m pip install -r requirements.txt
 echo "Fetching db.sqlite3 from GitHub"
 curl -o db.sqlite3 https://raw.githubusercontent.com/dixituwindsor/AdventureMinds/dixit/db.sqlite3
-chmod 777 db.sqlite3
+# Move it to a writable directory, such as /tmp
+mv db.sqlite3 /tmp/db.sqlite3
+
+# Modify the Django settings to point to this new location
+sed -i 's#NAME": "db.sqlite3"#NAME": "/tmp/db.sqlite3"#' AdventureMinds/settings.py
 python3 manage.py collectstatic --noinput --clear
 mv staticfiles_build/static public/
 echo "BUILD END"
